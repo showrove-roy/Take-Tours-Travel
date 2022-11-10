5101367aea62bd5ec11b091c5230746a1d1acb20
Showrove Roy <showrove200@gmail.com>
2022-11-10 14:36:52 +0600

    home page banner added

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,7 +1,29 @@
+import { Player } from "@lottiefiles/react-lottie-player";
 import React from "react";
 
 const Home = () => {
-  return <div className='mx-5'>This is home</div>;
+  return (
+    <div className=''>
+      <div className='md:-mt-5 lg:-mt-14 relative'>
+        <div className='absolute top-1/4 left-1/2 -translate-x-1/2 z-10 text-center'>
+          <h1
+            className='text-xl md:text-2xl lg:text-5xl uppercase font-bold text-[#EB6440]'
+            style={{ fontFamily: "'Exo 2', sans-serif" }}>
+            Welcome
+          </h1>
+          <h3
+            className='lg:text-4xl text-base md:text-2xl font-bold capitalize'
+            style={{ fontFamily: "'Exo 2', sans-serif" }}>
+            take tours <span className='text-[#EB6440]'> & </span> travel
+          </h3>
+        </div>
+        <Player
+          autoplay
+          loop
+          src='https://assets1.lottiefiles.com/packages/lf20_bhebjzpu.json'></Player>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
